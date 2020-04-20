56bad84727ea6814ee7e795d7d1fa36da96559d2
Ramon Tenório <ramon@nodementor.com>
2020-04-20 16:24:16 -0300

    Updates

---
+++ precache-manifest.1af1a12beb759fad2dcc5c56e0a0405c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6830e0636a3641667e7f2153ad6605b6",
+    "url": "/mnstr/index.html"
+  },
+  {
+    "revision": "681f8f533f4759a2663d",
+    "url": "/mnstr/static/css/main.983ae24a.chunk.css"
+  },
+  {
+    "revision": "1b94577a15796de21e98",
+    "url": "/mnstr/static/js/2.aa108c56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mnstr/static/js/2.aa108c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681f8f533f4759a2663d",
+    "url": "/mnstr/static/js/main.374b2e86.chunk.js"
+  },
+  {
+    "revision": "b64b643dd250145c1d72",
+    "url": "/mnstr/static/js/runtime-main.f8373226.js"
+  }
+]);
